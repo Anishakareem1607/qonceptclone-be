a519cd659f818bdf093f25b0b6755b5f198c3f1e
anisha <anishatk012@gmail.com>
2025-09-18 18:51:26 +0530

    message

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
-require('dotenv').config()
-const express=require('express')
-require('./DB/connection')
-const cors=require('cors')
-const contactRoutes=require('./Routes/contactRoutes')
-const contactServer=express()
-contactServer.use(cors())
-contactServer.use(express.json())
+// require('dotenv').config()
+// const express=require('express')
+// require('./DB/connection')
+// const cors=require('cors')
+// const contactRoutes=require('./Routes/contactRoutes')
+// const contactServer=express()
+// contactServer.use(cors())
+// contactServer.use(express.json())
 
-contactServer.use('/api/contacts',contactRoutes)
-const PORT=5000;
-contactServer.listen(PORT,()=>{
-    console.log(`server is running in ${PORT}`); 
-})
+// contactServer.use('/api/contacts',contactRoutes)
+// const PORT=5000;
+// contactServer.listen(PORT,()=>{
+//     console.log(`server is running in ${PORT}`); 
+// })
+
+require('dotenv').config();
+const express = require('express');
+require('./DB/connection');
+const cors = require('cors');
+const contactRoutes = require('./Routes/contactRoutes');
+
+const contactServer = express();
+
+
+contactServer.use(cors());
+contactServer.use(express.json());
+
+
+contactServer.use('/api/contacts', contactRoutes);
+
+contactServer.get('/', (req, res) => {
+  res.send('Backend is running... use /api/contacts');
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+contactServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
